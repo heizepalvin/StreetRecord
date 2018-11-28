c1bceb53aa2db129de4c4dc150d7a7640487def8
heizepalvin <palvin1993@naver.com>
2018-11-28 14:51:42 +0900

    Update NewMusicAdapter.java

--- app/src/main/java/com/example/heizepalvin/streetrecord/NewMusicAdapter.java
+++ app/src/main/java/com/example/heizepalvin/streetrecord/NewMusicAdapter.java
@@ -101,7 +101,6 @@ public class NewMusicAdapter extends BaseAdapter {
                         helper = new PlaylistDatabase(finalConvertView.getContext(),"playlist",null,version);
                         db = helper.getWritableDatabase();
                         int count = helper.search(db,items.get(position).getTitle());
-                        Log.e("테스트테스트테스트","count ? " + count);
                         if(count != 1){
                             helper.insert(db,items.get(position).getTitle(),items.get(position).getArtist(),items.get(position).getAlbumImg(),items.get(position).getMusicURL(),items.get(position).getLyrics(), items.get(position).getAlbumName(),items.get(position).getDate(),items.get(position).getGenre());
                             Log.e("newMusicAct","title ? " + items.get(position).getTitle());
@@ -112,7 +111,6 @@ public class NewMusicAdapter extends BaseAdapter {
                             helper.insert(db,items.get(position).getTitle(),items.get(position).getArtist(),items.get(position).getAlbumImg(),items.get(position).getMusicURL(),items.get(position).getLyrics(), items.get(position).getAlbumName(),items.get(position).getDate(),items.get(position).getGenre());
 
                             Toast.makeText(finalConvertView.getContext(), "중복되는 곡을 삭제하고 재생합니다.", Toast.LENGTH_SHORT).show();
-                            Log.e("테스트테스트테스트","들어옴 들어옴!");
                             Log.e("newMusicAct","title ? " + items.get(position).getTitle());
                             Log.e("newMusic","artist ? " + items.get(position).getArtist());
                             Log.e("newMusic","albumImg ? " + items.get(position).getAlbumImg());
@@ -125,7 +123,6 @@ public class NewMusicAdapter extends BaseAdapter {
 
                     }catch (SQLiteException e){
                         e.printStackTrace();
-                        Log.e("newMusicActDatabaseException","데이터베이스를 가져올 수 없음.");
                     }
                 } else {
                     Toast.makeText(finalConvertView.getContext(), "로그인을 해주세요.", Toast.LENGTH_SHORT).show();
